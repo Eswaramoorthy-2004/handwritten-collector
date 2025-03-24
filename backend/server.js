440e2a4718fe99cee597c46b262ae9f2b48bf9de
Eswaramoorthy-2004 <eswarmurugan143@gmail.com>
2025-03-24 23:18:04 +0530

    Added root route for backend

--- backend/server.js
+++ backend/server.js
@@ -57,3 +57,8 @@ app.get("/data", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
+
